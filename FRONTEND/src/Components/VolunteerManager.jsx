1085df068c4db71298d0d349410f6ffb2dc4a03e
Siri <padmasiri2805@gmail.com>
2025-11-04 13:41:02 +0530

    updated volunteermanager.jsx file

--- FRONTEND/src/Components/VolunteerManager.jsx
+++ FRONTEND/src/Components/VolunteerManager.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './style.css';
-import config from './config.js';
 
 const VolunteerManager = () => {
   const [volunteers, setVolunteers] = useState([]);
@@ -20,7 +19,7 @@ const VolunteerManager = () => {
   const [editMode, setEditMode] = useState(false);
   const [activeSection, setActiveSection] = useState('dashboard');
 
-  const baseUrl = `${config.url}/volunteerapi`;
+  const baseUrl = `${import.meta.env.VITE_API_URL}/volunteerapi`;
 
   useEffect(() => {
     if (activeSection === 'all') {
